a2d3c734a7ea7cc0cc3c2512f8076e3aa1209eff
axelnormand <axelnormand@gmail.com>
2020-02-06 00:15:43 +0000

    typing

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render, RenderResult, fireEvent} from '@testing-library/react-native';
 import {createModel} from '@xstate/test';
-import {getFeedbackMachine} from '../feedbackMachine';
+import {getFeedbackMachine, Context} from '../feedbackMachine';
 import {setTestMap} from './setTestMap';
 import App from '../App';
 
@@ -25,27 +25,29 @@ const feedbackMachine = setTestMap(getFeedbackMachine(), {
 });
 
 // add test action for each event
-export const feedbackModel = createModel(feedbackMachine).withEvents({
+export const feedbackModel = createModel<RenderResult, Context>(
+  feedbackMachine,
+).withEvents({
   CLICK_FEEDBACK: {
-    exec: ({getByTestId}: RenderResult<any>) => {
+    exec: ({getByTestId}) => {
       fireEvent.press(getByTestId('click_feedback'));
       jest.runAllTicks();
     },
   },
   CLICK_YES: {
-    exec: ({getByTestId}: RenderResult<any>) => {
+    exec: ({getByTestId}) => {
       fireEvent.press(getByTestId('click_yes'));
       jest.runAllTicks();
     },
   },
   CLICK_NO: {
-    exec: ({getByTestId}: RenderResult<any>) => {
+    exec: ({getByTestId}) => {
       fireEvent.press(getByTestId('click_no'));
       jest.runAllTicks();
     },
   },
   SUBMIT_FEEDBACK: {
-    exec: async ({getByTestId}: RenderResult<any>, event) => {
+    exec: async ({getByTestId}, event) => {
       // @ts-ignore event type for now
       fireEvent.changeText(getByTestId('input_feedback'), event.feedback);
       fireEvent.press(getByTestId('submit_feedback'));
@@ -54,13 +56,13 @@ export const feedbackModel = createModel(feedbackMachine).withEvents({
     cases: [{feedback: 'state machines ftw'}, {feedback: ''}],
   },
   CLICK_BACK: {
-    exec: ({getByTestId}: RenderResult<any>) => {
+    exec: ({getByTestId}) => {
       fireEvent.press(getByTestId('click_back'));
       jest.runAllTicks();
     },
   },
   CLICK_HOME: {
-    exec: ({getByTestId}: RenderResult<any>) => {
+    exec: ({getByTestId}) => {
       fireEvent.press(getByTestId('click_home'));
       jest.runAllTicks();
     },
